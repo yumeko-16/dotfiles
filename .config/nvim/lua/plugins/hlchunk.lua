f18b617e28dc43e43bdea8ceff259bfbc9ea8c88
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-01-22 00:50:40 +0700

    fix: Add a comma.

--- .config/nvim/lua/plugins/hlchunk.lua
+++ .config/nvim/lua/plugins/hlchunk.lua
@@ -24,7 +24,7 @@ return {
           style = {
             vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Whitespace")), "fg", "gui"),
           },
-        }
+        },
       })
     end
   },
