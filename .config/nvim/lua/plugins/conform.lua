e10a74f8855023673da70ae834f6db1adcf76071
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-01-24 20:29:27 +0700

    update: Delete unnecessary settings.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -3,7 +3,6 @@ return {
     'stevearc/conform.nvim',
     opts = {
       formatters_by_ft = {
-        lua = { "stylua" },
         javascript = { "prettier" },
         typescript = { "prettier" },
         javascriptreact = { "prettier" },
