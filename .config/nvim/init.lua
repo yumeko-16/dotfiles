38960364722909ddf2a1e9c4c816f371d824f9df
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-01-30 00:47:15 +0700

    clean: Optimize NeoVim config files.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,4 @@
+require("config.lazy")
 require("config.options")
-require("config.autocmds")
-
--- lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Plugins
-require("lazy").setup("plugins")
--- Keymaps
 require("config.keymaps")
+require("config.autocmds")
