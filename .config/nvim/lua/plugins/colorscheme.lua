af4ddf0f7ff8e077b0c1d9934e127cc68196505f
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-02-05 21:41:18 +0700

    add: Add diffview.nvim and change the color scheme.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,26 +1,13 @@
 return {
-  "cocopon/iceberg.vim",
+  "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    -- Apply colorscheme
-    vim.cmd.colorscheme("iceberg")
-
-    -- Force transparent backgrounds
-    local transparent_groups = {
-      "Normal",
-      "NormalFloat",
-      "SignColumn",
-      "EndOfBuffer",
-      "MsgArea",
-      "FloatBorder",
-      "LineNr",
-      "CursorLineNr",
-      "FoldColumn",
-    }
-
-    for _, group in ipairs(transparent_groups) do
-      vim.api.nvim_set_hl(0, group, { bg = "none" })
-    end
+  opts = {
+    style = "moon",
+    transparent = true,
+  },
+  config = function(_, opts)
+    require("tokyonight").setup(opts)
+    vim.cmd.colorscheme("tokyonight")
   end,
 }
