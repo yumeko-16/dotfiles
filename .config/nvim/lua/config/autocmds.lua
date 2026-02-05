e925fd1510dbcbd512d550e9cbd9d0e70e64c804
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-02-05 23:53:36 +0700

    change: Fix the issue where syntax highlighting does not work automatically in tsx.

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,24 +1,7 @@
-local augroup = vim.api.nvim_create_augroup("UserAutocmds", { clear = true })
-
-vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = function()
-    vim.api.nvim_set_hl(0, "Whitespace", {
-      fg = "#555555",
-      bg = "NONE",
-    })
-  end,
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = {
-    "html",
-    "javascript",
-    "lua",
-    "tsx",
-    "typescript",
-    "css",
-  },
-  callback = function()
-    pcall(vim.treesitter.start)
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function(args)
+    vim.schedule(function()
+      pcall(vim.treesitter.start, args.buf)
+    end)
   end,
 })
