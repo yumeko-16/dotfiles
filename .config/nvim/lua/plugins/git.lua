eac36d3cee9cd1856c106d1a8cbc2861562e7dc1
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-02-09 23:26:40 +0700

    change: Add the GitSignsCurrentLineBlame setting.

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,8 +1,14 @@
 return {
   {
     "lewis6991/gitsigns.nvim",
-    event = "BufReadPre",
-    config = true,
+    opts = function(_, opts)
+      opts.current_line_blame = true
+
+      vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", {
+        fg = "#565f89",
+        italic = true,
+      })
+    end,
   },
   {
     "tpope/vim-fugitive",
