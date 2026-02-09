b975f25ebb21a4e1c8b99a9d53241d07034936b9
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-02-09 23:15:27 +0700

    change: Specifies the background color of the diff.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,10 +8,10 @@ return {
     vim.cmd([[
       highlight Normal       guibg=NONE
       highlight NormalNC     guibg=NONE
-      highlight DiffAdd      guifg=NONE
-      highlight DiffChange   guifg=NONE
-      highlight DiffDelete   guifg=NONE
-      highlight DiffText     guifg=NONE
+      highlight DiffAdd      guifg=NONE guibg=#182a1e
+      highlight DiffChange   guifg=NONE guibg=#2a2113
+      highlight DiffDelete   guifg=NONE guibg=#2b1618
+      highlight DiffText     guifg=NONE guibg=#335c3b
     ]])
   end,
 }
