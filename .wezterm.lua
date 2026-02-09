c01dace4614c3d43ea7c8b0873dcda7e0990405b
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-02-09 21:07:22 +0700

    change: Eliminate spaces above, below, left and right of wezterm.

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,6 +15,12 @@ config.line_height = 1.4
 -- Background transparency
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 0
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 -- Color scheme
 config.color_scheme = "iceberg-dark"
