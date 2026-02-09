6c7dc8487a8f140210c4a3d07df45a798e6234e0
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-02-10 00:02:10 +0700

    change: Set the background color of DiffChange to None.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -9,9 +9,9 @@ return {
       highlight Normal       guibg=NONE
       highlight NormalNC     guibg=NONE
       highlight DiffAdd      guifg=NONE guibg=#182a1e
-      highlight DiffChange   guifg=NONE guibg=#2a2113
       highlight DiffDelete   guifg=NONE guibg=#2b1618
-      highlight DiffText     guifg=NONE guibg=#335c3b
+      highlight DiffChange   guifg=NONE guibg=NONE
+      highlight DiffText     guifg=NONE guibg=#2a2113
     ]])
   end,
 }
