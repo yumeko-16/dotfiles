0e9a609741b18189b8df6bc31793eaa3d860c020
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-02-11 22:08:10 +0700

    change: Change the color scheme to kanagawa dragon.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,16 @@
 return {
-  "cocopon/iceberg.vim",
+  "rebelot/kanagawa.nvim",
   lazy = false,
   priority = 1000,
-  config = function()
-    vim.cmd.colorscheme("iceberg")
-
-    vim.cmd([[
-      highlight Normal       guibg=NONE
-      highlight NormalNC     guibg=NONE
-      highlight DiffAdd      guifg=NONE guibg=#182a1e
-      highlight DiffDelete   guifg=NONE guibg=#2b1618
-      highlight DiffChange   guifg=NONE guibg=NONE
-      highlight DiffText     guifg=NONE guibg=#2a2113
-    ]])
+  opts = {
+    transparent = true,
+    theme = "dragon",
+    background = {
+      dark = "dragon",
+    },
+  },
+  config = function(_, opts)
+    require("kanagawa").setup(opts)
+    vim.cmd("colorscheme kanagawa")
   end,
 }
