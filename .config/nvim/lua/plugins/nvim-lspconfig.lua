d38c31f95bb7865876bc535fd425b37c08bbb7dc
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-02-21 02:04:14 +0700

    update: add lua_ls config to fix undefined global 'vim'.

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,3 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
+  vim.lsp.config("lua_ls", {
+    settings = {
+      Lua = {
+        diagnostics = {
+          globals = { "vim" },
+        },
+      },
+    },
+  }),
 }
