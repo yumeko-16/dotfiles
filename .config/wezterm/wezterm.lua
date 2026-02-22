0506c0449b17109af11d288c5d2b35401c713b04
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-02-22 14:39:19 +0700

    update: Sets the Wezterm decoration.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,19 +11,56 @@ config.font = wezterm.font("HackGen35 console NF", {
   stretch = "Normal",
 })
 config.line_height = 1.4
+config.use_ime = true
+
+-- Color scheme
+config.color_scheme = "iceberg-dark"
 
 -- Background transparency
 config.window_background_opacity = 0.95
 config.macos_window_background_blur = 0
+
+config.window_decorations = "RESIZE"
 config.window_padding = {
   left = 0,
   right = 0,
   top = 0,
   bottom = 0,
 }
+config.show_new_tab_button_in_tab_bar = false
+config.colors = {
+  tab_bar = {
+    inactive_tab_edge = "none",
+  },
+}
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
--- Color scheme
-config.color_scheme = "iceberg-dark"
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#5c6d74"
+  local foreground = "#FFFFFF"
+  local edge_background = "none"
+
+  if tab.is_active then
+    background = "#ae8b2d"
+    foreground = "#FFFFFF"
+  end
+
+  local edge_foreground = background
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_LEFT_ARROW },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Text = SOLID_RIGHT_ARROW },
+  }
+end)
 
 local act = require("wezterm").action
 config.keys = {
