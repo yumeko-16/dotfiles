f0b2acbd80cc5c55bae9934bd781796a13c2997e
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-03-01 02:35:51 +0700

    update: Enables the wrap option when using the Gdiffsplit command in NeoVim.

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -33,6 +33,17 @@ return {
   {
     "tpope/vim-fugitive",
     cmd = { "Git", "Gdiffsplit" },
+    config = function()
+      vim.api.nvim_create_autocmd("DiffUpdated", {
+        callback = function()
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            if vim.api.nvim_win_get_option(win, "diff") then
+              vim.api.nvim_win_set_option(win, "wrap", true)
+            end
+          end
+        end,
+      })
+    end,
   },
   {
     "kdheepak/lazygit.nvim",
