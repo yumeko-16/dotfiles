a3a77d54a5e0b2b8421f7c440df2820a767158ae
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-03-04 23:54:55 +0700

    clean: Change the order of functions.

--- .config/wezterm/on.lua
+++ .config/wezterm/on.lua
@@ -3,6 +3,30 @@ local is_windows = wezterm.target_triple == "x86_64-pc-windows-msvc"
 local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
 local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
+local function toggle(window, key, value)
+  local overrides = window:get_config_overrides() or {}
+
+  if overrides[key] == nil then
+    overrides[key] = value
+  else
+    overrides[key] = nil
+  end
+
+  window:set_config_overrides(overrides)
+end
+
+wezterm.on("toggle-opacity", function(window, _)
+  toggle(window, "window_background_opacity", 0.6)
+end)
+
+wezterm.on("toggle-blur", function(window, _)
+  if is_windows then
+    toggle(window, "win32_system_backdrop", "Disable")
+  else
+    toggle(window, "macos_window_background_blur", 0)
+  end
+end)
+
 wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
   local background = "#5c6d74"
   local foreground = "#ffffff"
@@ -27,28 +51,4 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
     { Foreground = { Color = edge_foreground } },
     { Text = SOLID_RIGHT_ARROW },
   }
-end)
-
-local function toggle(window, key, value)
-  local overrides = window:get_config_overrides() or {}
-
-  if overrides[key] == nil then
-    overrides[key] = value
-  else
-    overrides[key] = nil
-  end
-
-  window:set_config_overrides(overrides)
-end
-
-wezterm.on("toggle-opacity", function(window, _)
-  toggle(window, "window_background_opacity", 0.6)
-end)
-
-wezterm.on("toggle-blur", function(window, _)
-  if is_windows then
-    toggle(window, "win32_system_backdrop", "Disable")
-  else
-    toggle(window, "macos_window_background_blur", 0)
-  end
 end)
