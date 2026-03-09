611d2be9a8e485ea5b2ab1e88cd52f211b3b776f
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-03-09 22:15:05 +0700

    change: Disable the snacks dashboard.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -16,7 +16,6 @@ return {
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
-    dashboard = { enabled = true },
     indent = { enabled = true },
     scroll = { enabled = true },
     zen = {
