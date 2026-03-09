c61f222137278c23326afc46124cb08cdeb52662
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-03-10 00:08:28 +0700

    change: Change the highlight color.

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -11,10 +11,10 @@ return {
       vim.cmd([[
         highlight Normal       guibg=NONE
         highlight NormalNC     guibg=NONE
-        highlight DiffAdd      guifg=NONE guibg=#252e34
-        highlight DiffDelete   guifg=NONE guibg=#261c22
-        highlight DiffChange   guifg=NONE guibg=#1f2328
-        highlight DiffText     guifg=NONE guibg=#2a2f36
+        highlight DiffAdd      guifg=NONE guibg=#1f3f1f
+        highlight DiffDelete   guifg=NONE guibg=#5f0000
+        highlight DiffChange   guifg=NONE guibg=#3f464d
+        highlight DiffText     guifg=NONE guibg=#586069
       ]])
 
       vim.api.nvim_set_hl(0, "NotifyBackground", { bg = "#161821" })
