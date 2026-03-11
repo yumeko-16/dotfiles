6e0a6e69d4849907e2291110a2b1881b4b11d252
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-03-11 23:23:23 +0700

    update: Add the C compiler settings to use when using Windows.

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,6 +3,11 @@ return {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
     build = ":TSUpdate",
+    config = function()
+      if vim.fn.has("win32") == 1 and vim.fn.executable("zig") == 1 then
+        require("nvim-treesitter.install").compilers = { "zig" }
+      end
+    end,
   },
   {
     "windwp/nvim-ts-autotag",
