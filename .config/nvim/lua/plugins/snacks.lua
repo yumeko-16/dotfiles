ae0cfadcb2ed81222bc7cb3104920c4cadf09052
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-03-21 20:03:07 +0700

    update: Add a shortcut key for the picker.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -38,12 +38,41 @@ return {
       desc = "Buffers",
     },
     {
+      "<leader>fc",
+      function()
+        Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+      end,
+      desc = "Find Config File",
+    },
+    {
+      "<leader>ff",
+      function()
+        Snacks.picker.files()
+      end,
+      desc = "Find Files",
+    },
+    {
+      "<leader>fg",
+      function()
+        Snacks.picker.git_files()
+      end,
+      desc = "Find Git Files",
+    },
+    {
+      "<leader>fp",
+      function()
+        Snacks.picker.projects()
+      end,
+      desc = "Projects",
+    },
+    {
       "<leader>fr",
       function()
         Snacks.picker.recent()
       end,
       desc = "Recent",
     },
+    -- Other
     {
       "<leader>z",
       function()
