478cec237384623187483b832142b4513c904cc2
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-04-01 01:35:15 +0700

    clean: Remove stylelint's control from conform.lua.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,44 +1,18 @@
 return {
   "stevearc/conform.nvim",
-  opts = function()
-    local util = require("conform.util")
-
-    return {
-      formatters_by_ft = {
-        lua = { "stylua" },
-        javascript = { "prettier" },
-        typescript = { "prettier" },
-        javascriptreact = { "prettier" },
-        typescriptreact = { "prettier" },
-        css = { "stylelint", "prettier" },
-        scss = { "stylelint", "prettier" },
-        html = { "prettier" },
-      },
-
-      format_on_save = {
-        timeout_ms = 3000,
-      },
-
-      formatters = {
-        stylelint = {
-          command = "npx",
-          args = {
-            "stylelint",
-            "--fix",
-            "--allow-empty-input",
-            "--stdin",
-            "--stdin-filename",
-            "$FILENAME",
-          },
-          stdin = true,
-
-          condition = util.root_file({
-            ".stylelintrc",
-            ".stylelintrc.js",
-            "stylelint.config.js",
-          }),
-        },
-      },
-    }
-  end,
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      javascript = { "prettierd", "prettier", stop_after_first = true },
+      typescript = { "prettierd", "prettier", stop_after_first = true },
+      javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+      typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+      css = { "prettierd", "prettier", stop_after_first = true },
+      scss = { "prettierd", "prettier", stop_after_first = true },
+      html = { "prettierd", "prettier", stop_after_first = true },
+    },
+    format_on_save = {
+      timeout_ms = 3000,
+    },
+  },
 }
