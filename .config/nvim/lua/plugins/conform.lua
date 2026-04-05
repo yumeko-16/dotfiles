fdf43b485e08bfd09ba19628ef1fcb6b64097371
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-04-06 01:36:22 +0700

    update: Optimize the style lint settings.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -7,8 +7,8 @@ return {
       typescript = { "prettierd", "prettier", stop_after_first = true },
       javascriptreact = { "prettierd", "prettier", stop_after_first = true },
       typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-      css = { "prettierd", "prettier", stop_after_first = true },
-      scss = { "prettierd", "prettier", stop_after_first = true },
+      css = { "stylelint", "prettierd", "prettier" },
+      scss = { "stylelint", "prettierd", "prettier" },
       html = { "prettierd", "prettier", stop_after_first = true },
     },
     format_on_save = {
