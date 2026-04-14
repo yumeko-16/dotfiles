f1367857a44bb1d52e522a5d9e0fd4a7adf77893
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-04-14 23:18:37 +0700

    clean: Delete unnecessary comments.

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,9 +1,7 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
+  opts = {},
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
