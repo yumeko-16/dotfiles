63f3cc897aea39c671301e320d4ea5f0b3629eb7
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-05-21 19:27:19 +0700

    clean: Extract the settings for `gitsigns.nvim` from `git.lua`.

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,15 +1,5 @@
 return {
   {
-    "lewis6991/gitsigns.nvim",
-    opts = function(_, opts)
-      opts.current_line_blame = true
-      vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", {
-        fg = "#565f89",
-        italic = true,
-      })
-    end,
-  },
-  {
     "petertriho/nvim-scrollbar",
     opts = {
       handle = { color = "#3b3f4c" },
