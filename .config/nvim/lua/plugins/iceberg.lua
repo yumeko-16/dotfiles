fb3ecd0c7e5861f0791b8805a17b9776a8bdd600
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-06-06 17:47:05 +0700

    update: Define highlighting for gitgraph.nvim in iceberg.

--- .config/nvim/lua/plugins/iceberg.lua
+++ .config/nvim/lua/plugins/iceberg.lua
@@ -3,3 +3,10 @@ vim.cmd.colorscheme("iceberg")
 
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
+vim.api.nvim_set_hl(0, "GitGraphHash", { link = "Identifier" })
+vim.api.nvim_set_hl(0, "GitGraphAuthor", { link = "Type" })
+vim.api.nvim_set_hl(0, "GitGraphDate", { link = "Comment" })
+
+require("notify").setup({
+  background_colour = "#161821",
+})
