cb7cf394678c45bb144a19046b9c5d8cd4854512
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-06-06 18:08:09 +0700

    update: Define the branch highlighting color for iceberg's gitgraph.nvim.

--- .config/nvim/lua/plugins/iceberg.lua
+++ .config/nvim/lua/plugins/iceberg.lua
@@ -6,6 +6,10 @@ vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
 vim.api.nvim_set_hl(0, "GitGraphHash", { link = "Identifier" })
 vim.api.nvim_set_hl(0, "GitGraphAuthor", { link = "Type" })
 vim.api.nvim_set_hl(0, "GitGraphDate", { link = "Comment" })
+vim.api.nvim_set_hl(0, "GitGraphBranchName", {
+  fg = "#e4aa80",
+  bold = true,
+})
 
 require("notify").setup({
   background_colour = "#161821",
