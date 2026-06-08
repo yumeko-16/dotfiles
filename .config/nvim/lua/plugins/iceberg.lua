00592be8643fd42794305726e2e6fbd15c0ecd00
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-06-08 23:50:23 +0700

    update: Specify a highlight color for branch lines in gitgraph.

--- .config/nvim/lua/plugins/iceberg.lua
+++ .config/nvim/lua/plugins/iceberg.lua
@@ -10,6 +10,12 @@ vim.api.nvim_set_hl(0, "DiffText", { bg = "#3c3c5c", bold = true })
 vim.api.nvim_set_hl(0, "GitGraphHash", { link = "Identifier" })
 vim.api.nvim_set_hl(0, "GitGraphAuthor", { link = "Type" })
 vim.api.nvim_set_hl(0, "GitGraphDate", { link = "Comment" })
+vim.api.nvim_set_hl(0, "GitGraphBranch1", { fg = "#e27878" })
+vim.api.nvim_set_hl(0, "GitGraphBranch2", { fg = "#e2a478" })
+vim.api.nvim_set_hl(0, "GitGraphBranch3", { fg = "#b4be82" })
+vim.api.nvim_set_hl(0, "GitGraphBranch4", { fg = "#89b8c2" })
+vim.api.nvim_set_hl(0, "GitGraphBranch5", { fg = "#c6a0f6" })
+vim.api.nvim_set_hl(0, "GitGraphBranch6", { fg = "#f5c2e7" })
 vim.api.nvim_set_hl(0, "GitGraphBranchName", {
   fg = "#e4aa80",
   bold = true,
