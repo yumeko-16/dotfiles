5f883dd020b9b9f12ac60e3826f994fe8c06abab
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-06-17 01:24:44 +0700

    fix: Make stylelint executable on Windows.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -23,4 +23,12 @@ require("conform").setup({
       }
     end
   end,
+
+  formatters = {
+    stylelint = {
+      command = vim.fn.executable("stylelint") == 1 and "stylelint" or "stylelint.cmd",
+      args = { "--fix", "--stdin", "--stdin-filename", "$FILENAME" },
+      stdin = true,
+    },
+  },
 })
