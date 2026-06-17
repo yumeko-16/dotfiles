e020aca5647b2d29f116859189d5761bc8fdb0e8
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-06-18 00:29:41 +0700

    clean: Make the same code a variable.

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -1,13 +1,15 @@
+local prettier = { "prettierd", "prettier", stop_after_first = true }
+
 require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-    typescript = { "prettierd", "prettier", stop_after_first = true },
-    javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-    css = { "prettierd", "prettier", stop_after_first = true },
-    scss = { "prettierd", "prettier", stop_after_first = true },
-    html = { "prettierd", "prettier", stop_after_first = true },
+    javascript = prettier,
+    typescript = prettier,
+    javascriptreact = prettier,
+    typescriptreact = prettier,
+    css = prettier,
+    scss = prettier,
+    html = prettier,
   },
 
   format_on_save = {
