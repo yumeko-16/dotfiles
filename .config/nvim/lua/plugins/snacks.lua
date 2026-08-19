3ad90c82d1e8262124506afc0cde20d5bbddf850
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-08-19 22:54:36 +0700

    update: Set the keymap for grep search of snacks.nvim.

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -13,11 +13,31 @@ snacks.setup({
   },
 })
 
+-- find
 vim.keymap.set("n", "<leader>fb", snacks.picker.buffers, { desc = "Buffers" })
+
 vim.keymap.set("n", "<leader>fc", function()
   snacks.picker.files({ cwd = vim.fn.stdpath("config") })
 end, { desc = "Find Config File" })
+
 vim.keymap.set("n", "<leader>ff", snacks.picker.files, { desc = "Find Files" })
 vim.keymap.set("n", "<leader>fg", snacks.picker.git_files, { desc = "Find Git Files" })
 vim.keymap.set("n", "<leader>fp", snacks.picker.projects, { desc = "Projects" })
 vim.keymap.set("n", "<leader>fr", snacks.picker.recent, { desc = "Recent" })
+
+-- Grep
+vim.keymap.set("n", "<leader>sb", function()
+  snacks.picker.lines()
+end, { desc = "Buffer Lines" })
+
+vim.keymap.set("n", "<leader>sB", function()
+  snacks.picker.grep_buffers()
+end, { desc = "Grep Open Buffers" })
+
+vim.keymap.set("n", "<leader>sg", function()
+  snacks.picker.grep()
+end, { desc = "Grep" })
+
+vim.keymap.set({ "n", "x" }, "<leader>sw", function()
+  snacks.picker.grep_word()
+end, { desc = "Visual selection or word" })
