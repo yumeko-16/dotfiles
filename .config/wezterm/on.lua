b2477f6ced53baf8fb2add19893b1de12549cc4c
tomohiro.maeno <tomohiro.maeno.dev@gmail.com>
2026-09-26 01:22:23 +0700

    change: Restore Wezterm's tab style to default.

--- .config/wezterm/on.lua
+++ .config/wezterm/on.lua
@@ -1,6 +1,4 @@
 local wezterm = require("wezterm")
-local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
-local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
 
 local function toggle(window, key, value)
   local overrides = window:get_config_overrides() or {}
@@ -16,30 +14,4 @@ end
 
 wezterm.on("toggle-opacity", function(window, _)
   toggle(window, "window_background_opacity", 0.9)
-end)
-
-wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-  local background = "#5c6d74"
-  local foreground = "#ffffff"
-  local edge_background = "none"
-
-  if tab.is_active then
-    background = "#ae8b2d"
-    foreground = "#ffffff"
-  end
-
-  local edge_foreground = background
-  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
-
-  return {
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_LEFT_ARROW },
-    { Background = { Color = background } },
-    { Foreground = { Color = foreground } },
-    { Text = title },
-    { Background = { Color = edge_background } },
-    { Foreground = { Color = edge_foreground } },
-    { Text = SOLID_RIGHT_ARROW },
-  }
 end)
